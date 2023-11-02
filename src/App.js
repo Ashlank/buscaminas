8c1ff95dc33ecf54baea7adb9dee95f7dbe96a68
Gastón Alvarez <gaston_a96@hotmail.com>
2023-11-02 19:43:04 -0300

    adding heading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Container from './componentes/Container';
 
 function App() {
   return (
-    <div className="">
+    <div className="text-center">
+      <h1 className='text-3xl font-bold mt-6'>Buscaminas</h1>
       <Container />
     </div>
   );
